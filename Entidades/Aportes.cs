f1bcdda8254b60840ac003fd4e530369444a26e9
Danilo Gabriel Bonifacio Bautista <danilo_bonifacio@ucne.edu.do>
2021-10-04 22:19:55 -0400

    Creacion de la entidad y los atributos de la misma

---
+++ Entidades/Aportes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Danilo_20190266.Entidades
+{
+    public class Aportes
+    {
+        [Key]
+        public int AporteID { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public String Persona { get; set; }
+        public String Concepto { get; set; }
+        public int Monto { get; set; }
+    }
+}
