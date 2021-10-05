51c541f09faf64377f2e126c06534e664d3eec32
Danilo Gabriel Bonifacio Bautista <danilo_bonifacio@ucne.edu.do>
2021-10-04 22:31:16 -0400

    Creacion del contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,22 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using P1_AP1_Danilo_20190266.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P1_AP1_Danilo_20190266.DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Aportes> Aportes { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(@"Data Source = DATA\TeacherControl.db");
+        }
+    }
+}
